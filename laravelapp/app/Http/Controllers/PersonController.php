b890418fa04bebf9f635564692e5a7db67e72995
gotou-katsuki <github0606>
2022-03-23 14:00:47 +0900

    3/23

--- laravelapp/app/Http/Controllers/PersonController.php
+++ laravelapp/app/Http/Controllers/PersonController.php
@@ -9,28 +9,33 @@ use Illuminate\Database\Eloquent\Model;
 
     class PersonController extends Controller
 {
+   public function edit(Request $request)
+{
+   $person = Person::find($request->id);
+   return view('person.edit', ['form' => $person]);
+}
+
+public function update(Request $request)
+{
+   $this->validate($request, Person::$rules);
+   $person = Person::find($request->id);
+   $form = $request->all();
+   unset($form['_token']);
+   $person->fill($form)->save();
+   return redirect('/person');
+}
+
+public function delete(Request $request)
+{
+   $person = Person::find($request->id);
+   return view('person.del', ['form' => $person]);
+}
+
 public function index(Request $request)
 {
-   $items = Person::all();
-   return view('person.index', ['items' => $items]);
-}
-public function getData()
-{
-   return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
-}
-
-public function find(Request $request)
-{
-   return view('person.find',['input' => '']);
-}
-
-public function search(Request $request)
-{
-   $item = Person::find($request->input);
-   $param = ['input' => $request->input, 'item' => $item];
-   return view('person.find', $param);
+   $hasItems = Person::has('boards')->get();
+   $noItems = Person::doesntHave('boards')->get();
+   $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
+   return view('person.index', $param);
 }
 }
-
-
-
